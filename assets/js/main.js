a65bbace002b695d1621334f445177551b98783b
unknown <ankit9sinha@gmail.com>
2020-08-03 10:42:47 -0500

    Added a new section for Achievements with custom scrollbar and modified Projects section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,12 @@
 jQuery(document).ready(function($) {
     
-	$(".project").hide();
+	// Create custom scrollbar for Achievements section
+	$(".scrollBox").customScrollbar({
+		fixedThumbHeight:50,
+		fixedThumbWidth:10,
+		animationSpeed:0
+	});
 	
-	// Show first 4 projects
-    $(".project").slice(0, 4).show();
-    
-	// Show next 4 hidden projects
-	$("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        $(".project:hidden").slice(0, 4).slideDown();
-        
-		// Disable the load more button if no more hidden projects
-		if ($(".project:hidden").length == 0) {
-            $("#loadMore").prop('disabled', true);
-			$("#loadMore").css('background-color', '#DAE3E7');
-        }
-        $('html,body').animate({
-            scrollTop: $(this).offset().top
-        }, 1500);
-    });
-    
 	// Show and hide the arrow for scrolling
 	$(window).scroll(function(){ 
         if ($(this).scrollTop() > 100) { 
